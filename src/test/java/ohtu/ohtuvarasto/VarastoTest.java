d881c78a1489742ae0ebd62bb81db12b39a614ed
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2020-10-26 17:28:56 +0200

    Test coverage 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,6 +1,7 @@
 package ohtu.ohtuvarasto;
 
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -8,6 +9,9 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class VarastoTest {
@@ -63,6 +67,77 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaLiikaaVarastosta() {
+        varasto.lisaaVarastoon(5);
+        varasto.otaVarastosta(110);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaNegatiivinenMaara() {
+        varasto.lisaaVarastoon(1);
+        varasto.otaVarastosta(-10);
+
+        assertEquals(1, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylimaaraMeneeHukkaan() {
+        varasto.lisaaVarastoon(varasto.getTilavuus() * 10);
+        assertEquals(varasto.getTilavuus(), varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaNegatiivinenMaara() {
+        double ed = varasto.getSaldo();
+        varasto.lisaaVarastoon(-10);
+
+        assertEquals(ed, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void mahdotonTilavuus() {
+        Varasto v = new Varasto(-100);
+
+        assertEquals(0, v.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void kuormitettuKonstruktoriOikeaTilavuus() {
+        Varasto v = new Varasto(10, 100);
+
+        assertEquals(v.getTilavuus(), 10, vertailuTarkkuus);
+    }
+
+    @Test
+    public void kkMahdotonTilavuus() {
+        Varasto v = new Varasto(-10, 100);
+
+        assertEquals(0, v.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldoNegatiivinen() {
+        Varasto v = new Varasto(10, -10);
+        assertEquals(0, v.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldoSamaKuinSaldo() {
+        Varasto v = new Varasto(10, 6);
+        assertEquals(6, v.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void tulostusmuotoOikea() {
+        String tulos = varasto.toString().replaceAll("[0-9\\.]", "");
+        String oikea = "saldo = 10, vielä tilaa 10".replaceAll("[0-9\\.]", "");
+
+        assertTrue("oikea:" + oikea + "    tulos:" + tulos, oikea.equals(tulos));
     }
 
 }
