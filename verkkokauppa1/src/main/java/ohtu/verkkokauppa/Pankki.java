f6be856f34032ad910a422d2b7b94ee07c5413d2
HoolaBoola <jaime.heikkiladias@helsinki.fi>
2020-11-09 16:40:49 +0200

    Spring injection working, exercises for week 2 done

--- verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,13 +1,14 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Pankki implements PankkiInterface {
 
+    @Autowired
     private KirjanpitoInterface kirjanpito;
-
-    public Pankki(KirjanpitoInterface kirjanpito) {
-        this.kirjanpito = kirjanpito;
-    }
-
+    
     @Override
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
         kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
